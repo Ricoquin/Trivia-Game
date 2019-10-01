a6ad4df2547fee5d02bb10cc57fe716d1cc02a6e
Ricardo Quintanilla <rqchicago.rq@gmail.com>
2019-10-01 15:28:52 -0500

    questions/on click functions added

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,3 +1,50 @@
 $('#start').on('click',function(){
-    console.log("YOU CLICKED ME!")
+    for(var i=0;i<questions.length;i++){
+        $('#subWrapper').append('<h2>' +questions[i].question+'</h2')
+        for(var j=0;j<questions[i].answers.length;j++){
+            $("")
+        }  
+    }
 })
+
+var questions = [{
+        question : "first question", 
+        answers : [ "answer one", "answer two", "answer three", "answer four" ],
+        correctAnswer : "correct answer"
+    }, {
+        question : "second question", 
+        answers : [ "answer one", "answer two", "answer three", "answer four" ],
+        correctAnswer : "correct answer"
+    }, {
+        question : "third question", 
+        answers : [ "answer one", "answer two", "answer three", "answer four" ],
+        correctAnswer : "correct answer"
+    }, {
+        question : "fourth question", 
+        answers : [ "answer one", "answer two", "answer three", "answer four" ],
+        correctAnswer : "correct answer"
+    }, {
+        question : "fifth question", 
+        answers : [ "answer one", "answer two", "answer three", "answer four" ],
+        correctAnswer : "correct answer"
+    }, {
+        question : "Sixth question", 
+        answers : [ "answer one", "answer two", "answer three", "answer four" ],
+        correctAnswer : "correct answer"
+    }, {
+        question : "Seventh question", 
+        answers : [ "answer one", "answer two", "answer three", "answer four" ],
+        correctAnswer : "correct answer"
+    }, {
+        question : "Eighth question", 
+        answers : [ "answer one", "answer two", "answer three", "answer four" ],
+        correctAnswer : "correct answer"
+    }, {
+        question : "ninth question", 
+        answers : [ "answer one", "answer two", "answer three", "answer four" ],
+        correctAnswer : "correct answer"
+    }, {
+        question : "tenth question", 
+        answers : [ "answer one", "answer two", "answer three", "answer four" ],
+        correctAnswer : "correct answer"
+    }];
