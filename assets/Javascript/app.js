b22b2412e3b223ba000812109d0611bc91339917
Ricardo Quintanilla <rqchicago.rq@gmail.com>
2019-10-08 20:51:48 -0500

    Updated questions and timer

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -2,51 +2,75 @@ $('#start').on('click',function(){
     for(var i=0;i<questions.length;i++){ // 1st quest 
         $('#startButton').append('<h2>' + questions[i].question+'</h2>')
         for(var j=0;j<questions[i].answers.length;j++){
-            $("#startButton").append("<input type='radio' name='question-"+i+"' value='"+questions[i].answers[j]+"'><p>" + questions[j].answers[j]+ "</p>")
+            $("#startButton").append("<input type='radio' name='question-"+i+"' value='"+questions[i].answers[j]+"'><p>" + questions[i].answers[j]+ "</p>")
         }  
     }
 })
 // questions for the quiz //
 var questions = [{
-        question : "first question", 
-        answers : [ "answer one", "answer two", "answer three", "answer four" ],
-        correctAnswer : "correct answer"
+        question : "Nintendo is the Japanese word meaning..", 
+        answers : [ "Fun Television Gamme", "Too much excitement", "Fun home entertainment", "Leave luck to heaven" ],
+        correctAnswer : "Leave Luck to Heaven"
     }, {
-        question : "second question", 
-        answers : [ "answer one", "answer two", "answer three", "answer four" ],
-        correctAnswer : "correct answer"
+        question : "Kenji Yamamoto composed all these games except..", 
+        answers : [ "Mike Tyson's Punch-Out!!", "Super Smash Bros.", "Super Metroid", "Super Mario Bros." ],
+        correctAnswer : "Super Mario Bros."
     }, {
-        question : "third question", 
-        answers : [ "answer one", "answer two", "answer three", "answer four" ],
-        correctAnswer : "correct answer"
+        question : "The most common species in World of Warcraft is ", 
+        answers : [ "Orcs", "Humans", "Night Elves", "dwarves" ],
+        correctAnswer : "Humans"
     }, {
-        question : "fourth question", 
-        answers : [ "answer one", "answer two", "answer three", "answer four" ],
-        correctAnswer : "correct answer"
+        question : "Who has a appeard on the most covers for EA Sports Fifa.. ", 
+        answers : [ "Lionel Messi", "Cristiano Rinaldo", "Wayne Rooney", "Landon Donovan" ],
+        correctAnswer : "Wayne Rooney"
     }, {
-        question : "fifth question", 
-        answers : [ "answer one", "answer two", "answer three", "answer four" ],
-        correctAnswer : "correct answer"
+        question : "In Uncharted's series who was his sidekick and close friend ?", 
+        answers : [ "Harry Flynn", "Elena Fisher", "Victor Sullivan", "Charlie Cutter" ],
+        correctAnswer : "Victor Sullivan also known as Sully"
     }, {
-        question : "Sixth question", 
-        answers : [ "answer one", "answer two", "answer three", "answer four" ],
-        correctAnswer : "correct answer"
+        question : "When was the first Call of Duty title released?", 
+        answers : [ "December 1, 2003", "November 14 2003", "October 29, 2003", "July 18, 2004" ],
+        correctAnswer : "October 29, 2003. Most games are relased late November through Early December"
     }, {
-        question : "Seventh question", 
-        answers : [ "answer one", "answer two", "answer three", "answer four" ],
-        correctAnswer : "correct answer"
+        question : "Most played game in the worldwide is ..", 
+        answers : [ "Fortnite", "Minecraft", "World of Warcraft", "League of Legends" ],
+        correctAnswer : "Correct ! League of Legends is played worldwide with gamers have logged in over 1.3 billion hours of game play or 148401 years !! Yowza"
     }, {
-        question : "Eighth question", 
-        answers : [ "answer one", "answer two", "answer three", "answer four" ],
-        correctAnswer : "correct answer"
+        question : "Most played gaming console in the world", 
+        answers : [ "Android Apps", "Playstation 4", "PC", "Nintendo Switch" ],
+        correctAnswer : "Correct! PC is the most played console, obvious right ? "
     }, {
-        question : "ninth question", 
-        answers : [ "answer one", "answer two", "answer three", "answer four" ],
-        correctAnswer : "correct answer"
+        question : "Highest grossed revenue made by what game ?", 
+        answers : [ "PlayerUnknown's Battlegrounds", "FIFA 18", "Grand Theft Auto V", "Call of Duty: Black Ops IIII" ],
+        correctAnswer : "Correct! PlayerUnknown's Battlegrounds, Bluehole  at a WHOPPING $1.028 billion"
     }, {
-        question : "tenth question", 
-        answers : [ "answer one", "answer two", "answer three", "answer four" ],
-        correctAnswer : "correct answer"
+        question : "The Division 2 sequal to Tom Clancy's The Division can be finished in how many hours? ", 
+        answers : [ "35 hours", "65 hours", "40 hours", "80 hours" ],
+        correctAnswer : "Correct! 40 hours is Approximate and usually will take more if you can resist the awesome side missions"
     }];
 
+    $("#startButton").on("click", run);
+
+      function run() {
+      clearInterval(intervalId);
+      intervalId = setInterval(decrement, 25000);
+    }
+
+    function decrement() {
+        number--;
+    $("#show-number").html("<h1>" + number + "</h1>");
     
+    if (number === 0) {
+        stop();
+        alert("Time Up!");
+      }
+    }
+    function stop() {
+      clearInterval(intervalId);
+    }
+
+    run();
+
+
+
+document.body.style.backgroundImage = "url('https://r3.whistleout.com/public/images/articles/2017/05/Missouri.png')";
